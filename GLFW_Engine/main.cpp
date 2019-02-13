8c8f25d29706a8f9b06f224a6c06bdd46b154290
JoshFlash <joshuaflash@gmail.com>
2019-02-13 21:44:33 +1300

    start work on draw triangle

--- GLFW_Engine/main.cpp
+++ GLFW_Engine/main.cpp
@@ -6,7 +6,32 @@
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
+GLuint VAO, VBO, shader;
+
 GLFWwindow* initMainWindowContext();
+
+void drawTriangle()
+{
+	GLfloat vertices[] = 
+	{
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+			glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
 
 int main(int argc, char *args[])
 {
